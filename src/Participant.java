c388600db48cd6d58fa897cefc38a313fc5b181e
IAmJeremiah <jtcoleman10@gmail.com>
2022-11-16 19:04:29 -0600

    jeremiah's survey

--- src/Participant.java
+++ src/Participant.java
@@ -7,6 +7,11 @@ public class Participant {
     total number of female participants -> variable name can be totalNumberOfFemaleParticipants
      */
 
+    public static int totalNumberOfParticipants;
+    public static int totalNumberofMaleParticipants;
+    public static int totalNumberofFemaleParticipants;
+
+
 
     /*
     Declare instance variables for each participant as below
@@ -15,19 +20,43 @@ public class Participant {
     gender -> can be char
      */
 
+    public String name;
+    public int age;
+    public char gender;
+
+    public Participant(String name, int age, char gender) {
+    }
 
 
     //Create a static void method addParticipants() that will increase totalNumberOfParticipants by 1
+    public static void addParticipants(){
+        totalNumberOfParticipants++;
+    }
 
 
     //Create a static void method addMaleParticipants() that will increase totalNumberOfMaleParticipants by 1
 
+    public static void addMaleParticipants(){
+        totalNumberofMaleParticipants++;
+    }
+
 
     //Create a static void method addFemaleParticipants() that will increase totalNumberOfFemaleParticipants by 1
+    public static void addFemaleParticipants(){
+        totalNumberofFemaleParticipants++;
+    }
 
 
 
     //Override toString() method and implement a new body that will return all information about a Participant
 
 
+    @Override
+    public String toString() {
+        return "Participant{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
 }
--- src/Survey.java
+++ src/Survey.java
@@ -1,3 +1,7 @@
+import com.sun.xml.internal.ws.wsdl.writer.document.Part;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Survey {
     public static void main(String[] args) {
@@ -14,6 +18,51 @@ public class Survey {
         -Print the age of the youngest person
         -Print the age of the eldest person
          */
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Participant> participants = new ArrayList<>();
+        int maxAge = 0, minAge = Integer.MAX_VALUE;
+
+        //3
+        do {
+
+            System.out.println(SurveyQuestions.askToJoin);
+            String answer1 = scanner.nextLine();
+
+            if(answer1.toLowerCase().startsWith("y")){
+                // Getting current participant's name
+                System.out.println(SurveyQuestions.askName);
+                String name = scanner.nextLine();
+
+                System.out.println(SurveyQuestions.askAge);
+                int age = scanner.nextInt();
+                scanner.nextLine(); // cleaning the empty line
+
+                // finding the max and min age out of all participants
+                if (age > maxAge) maxAge = age;
+                if (age < minAge) minAge = age;
+
+                System.out.println(SurveyQuestions.askGender);
+                char gender = scanner.nextLine().toUpperCase().charAt(0);
+
+                Participant participant = new Participant(name, age, gender);
+                participants.add(participant);
+
+                if (gender == 'M') Participant.addMaleParticipants();
+                else Participant.addFemaleParticipants();
+            }
+
+        }while (Participant.totalNumberOfParticipants < 3);
+
+        for (Participant p: participants){
+            System.out.println(p);
+        }
+
+        System.out.println("The total number of male participants is = " + Participant.totalNumberofMaleParticipants);
+        System.out.println("The total number of male participants is = " + Participant.totalNumberofFemaleParticipants);
+
+        System.out.println("The age of the youngest person is = " + minAge);
+        System.out.println("The age of the youngest person is = " + maxAge);
 
     }
 }
